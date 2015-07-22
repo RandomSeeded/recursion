69929c3a11a1c69c5f51d341b00816ac76cac365
Nate Willard <nate.a.willard@gmail.com>
2015-07-21 18:34:26 -0700

    getElementsByClassName implemented

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, element) {
+  // Basic concept: we're going to traverse the entire document.body as if it were a tree
+  // And we're going to return up an array of all the stuff that matched
+
+  // initialize element for initial pass
+  if (element === undefined) { element = document.body; }
+
+  // base case - the element has no children
+  if (! element.hasChildNodes()) {
+    if (element.classList !== undefined && element.classList.contains(className)) { return [element]; }
+    else return [];
+  }
+
+  // recursive case - the element has children
+  else {
+    var matches = [];
+    if (element.classList.contains(className)) { matches.push(element); }
+    var children = element.childNodes;
+    for (var i = 0; i < children.length; i++) {
+      var result = getElementsByClassName(className, children[i]);
+      matches = Array.prototype.concat(matches, result);
+    }
+    return matches;
+  }
 };
